c29d4e05f362188a4d68ab61bee4991451cbff12
hellraisernav <nowaratall@gmail.com>
2024-02-18 08:50:33 +0330

    First Commit

---
+++ portfolio/src/client.js
@@ -0,0 +1,5 @@
+import sanityClient from '@sanity/client'
+export default sanityClient({
+    projetctId:"c1cdy6c3",
+    dataset:"production"
+})
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c1cdy6c3',
+    dataset: 'production'
+  }
+})
